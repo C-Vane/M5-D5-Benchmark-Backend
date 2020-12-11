60baa0b96289e34ec42e83c320ab234ba361abfb
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-12-11 16:40:47 +0100

    ss

--- src/products/index.js
+++ src/products/index.js
@@ -50,8 +50,8 @@ router.get("/:id", async (req, res, next) => {
 router.post(
   "/",
   [
-    check("Name").exists().isLength({ min: 1 }).withMessage("Give it a name"),
-    check("Description").exists().isLength({ min: 1 }).withMessage("Give it a description"),
+    check("name").exists().isLength({ min: 1 }).withMessage("Give it a name"),
+    check("description").exists().isLength({ min: 1 }).withMessage("Give it a description"),
     check("brand").exists().isLength({ min: 1 }).withMessage("You have to give a brand for the product"),
     check("price").exists().isLength({ min: 1 }).withMessage("You need to set your price"),
   ],
@@ -74,33 +74,42 @@ router.post(
   }
 );
 
-router.put("/:id", async (req, res, next) => {
-  try {
-    const productDataBase = await readDB(productFilePath); //RUNS FUNCTION TO GET DATABASE
-    const singleProduct = productDataBase.filter((product) => product.ID === req.params.id);
-    if (singleProduct.length > 0) {
-      const filteredDB = productDataBase.filter((product) => product.ID !== req.params.id);
-      console.log(singleProduct);
-      const editedProduct = {
-        ...req.body,
-        ID: singleProduct[0].ID,
-        StudentID: singleProduct[0].StudentID,
-        CreationDate: singleProduct[0].CreationDate,
-        ModifiedDate: new Date(),
-      };
-      filteredDB.push(editedProduct);
-      await writeDB(productFilePath, filteredDB);
-      res.status(201).send(filteredDB); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
-    } else {
-      const err = {};
-      err.httpStatusCode = 404;
-      next(err);
+router.put(
+    "/:id",
+    [
+        check("name").exists().isLength({ min: 1 }).withMessage("Give it a name"),
+        check("description").exists().isLength({ min: 1 }).withMessage("Give it a description"),
+        check("brand").exists().isLength({ min: 1 }).withMessage("You have to give a brand for the product"),
+        check("price").exists().isLength({ min: 1 }).withMessage("You need to set your price"),
+    ],
+    async (req, res, next) => {
+      try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+          const err = new Error();
+          err.message = errors.errors.length > 0 || "Student ID not Correct";
+          err.httpStatusCode = 400;
+          next(err);
+        } else {
+          const productsDB = await readDB(productFilePath);
+          const newDb = productsDB.filter((product) => product.ID !== req.params.id);
+  
+          const modifiedproduct = {
+            ...req.body,
+            ID: req.params.id,
+            modifiedDate: new Date(),
+          };
+  
+          newDb.push(modifiedproduct);
+          await writeDB(productFilePath, newDb);
+  
+          res.send({ id: modifiedproduct.ID });
+        }
+      } catch (error) {
+        next(error);
+      }
     }
-  } catch (err) {
-    err.httpStatusCode = 404;
-    next(err);
-  }
-});
+  );
 
 router.delete("/:id", async (req, res, next) => {
   try {
