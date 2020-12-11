585ce0cce4fbae78188e25eb066d3c09dab2b2e4
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2020-12-11 12:08:32 +0100

    Add catagory search

--- src/products/index.js
+++ src/products/index.js
@@ -13,7 +13,12 @@ router.get("/", async (req, res, next) => {
   try {
     const productDataBase = await readDB(productFilePath); //RUNS FUNCTION TO GET DATABASE
     if (productDataBase.length > 0) {
-      res.status(201).send(productDataBase); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
+      if (req.query && req.query.category) {
+        const filteredproducts = productDataBase.filter((product) => product.hasOwnProperty("category") && product.category.toLowerCase() === req.query.category.toLowerCase());
+        res.send(filteredproducts);
+      } else {
+        res.status(201).send(productDataBase); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
+      }
     } else {
       const err = {};
       err.httpStatusCode = 404;
@@ -28,9 +33,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     const productDataBase = await readDB(productFilePath); //RUNS FUNCTION TO GET DATABASE
-    const singleProduct = productDataBase.filter(
-      (product) => product.ID === req.params.id
-    );
+    const singleProduct = productDataBase.filter((product) => product.ID === req.params.id);
     if (singleProduct.length > 0) {
       res.status(201).send(singleProduct); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
     } else {
@@ -47,22 +50,10 @@ router.get("/:id", async (req, res, next) => {
 router.post(
   "/",
   [
-    check("Name")
-      .exists()
-      .isLength({ min: 1 })
-      .withMessage("Give it a name"),
-    check("Description")
-      .exists()
-      .isLength({ min: 1 })
-      .withMessage("Give it a description"),
-    check("brand")
-      .exists()
-      .isLength({ min: 1 })
-      .withMessage("You have to give a brand for the product"),
-    check("price")
-      .exists()
-      .isLength({ min: 1 })
-      .withMessage("You need to set your price"),
+    check("Name").exists().isLength({ min: 1 }).withMessage("Give it a name"),
+    check("Description").exists().isLength({ min: 1 }).withMessage("Give it a description"),
+    check("brand").exists().isLength({ min: 1 }).withMessage("You have to give a brand for the product"),
+    check("price").exists().isLength({ min: 1 }).withMessage("You need to set your price"),
   ],
   async (req, res, next) => {
     const errors = validationResult(req);
@@ -86,13 +77,9 @@ router.post(
 router.put("/:id", async (req, res, next) => {
   try {
     const productDataBase = await readDB(productFilePath); //RUNS FUNCTION TO GET DATABASE
-    const singleProduct = productDataBase.filter(
-      (product) => product.ID === req.params.id
-    );
+    const singleProduct = productDataBase.filter((product) => product.ID === req.params.id);
     if (singleProduct.length > 0) {
-      const filteredDB = productDataBase.filter(
-        (product) => product.ID !== req.params.id
-      );
+      const filteredDB = productDataBase.filter((product) => product.ID !== req.params.id);
       console.log(singleProduct);
       const editedProduct = {
         ...req.body,
@@ -118,13 +105,9 @@ router.put("/:id", async (req, res, next) => {
 router.delete("/:id", async (req, res, next) => {
   try {
     const productDataBase = await readDB(productFilePath); //RUNS FUNCTION TO GET DATABASE
-    const singleProduct = productDataBase.filter(
-      (product) => product.ID === req.params.id
-    );
+    const singleProduct = productDataBase.filter((product) => product.ID === req.params.id);
     if (singleProduct.length > 0) {
-      const filteredDB = productDataBase.filter(
-        (product) => product.ID !== req.params.id
-      );
+      const filteredDB = productDataBase.filter((product) => product.ID !== req.params.id);
       await writeDB(productFilePath, filteredDB);
       res.status(201).send(filteredDB); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
     } else {
