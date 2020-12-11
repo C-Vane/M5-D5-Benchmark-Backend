b2a8388ddbac78e5d73811e8fe45517f8df04249
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-12-11 15:06:09 +0100

    lol

--- src/files/index.js
+++ src/files/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const multer = require("multer");
 const { writeFile, createReadStream } = require("fs-extra");
-const { join } = require("path");
+const { join} = require("path");
+const path = require("path")
 const { response } = require("express");
+const { readDB, writeDB } = require("../../lib/utilities");
 
 const router = express.Router();
 
 const upload = multer({});
-const ProductFilePath = join(__dirname, "./products/products.json");
+const ProductFilePath = join(__dirname, "../products/products.json");
+console.log(ProductFilePath)
 const checkProduct = async (id) => {
 const products = await readDB(ProductFilePath);
 const productExsists = products.filter((product) => product.ID === id);
@@ -18,14 +21,27 @@ router.post("/:id/uploadPhoto", upload.single("productImg"), async (req, res, ne
   console.log(req.params.id);
   console.log(req.file.originalname);
   try {
+
     //   if(checkProduct(req.params.id)){
     //     console.log(true)
     //    res.status(400).send("something")
     //   } else{
           await writeFile(
-      join(productImagePath, req.file.originalname),
+      join(productImagePath, req.params.id + path.extname(req.file.originalname)),
       req.file.buffer
+
+
     );
+
+    let oldDb = await readDB(ProductFilePath)
+    let newDb = oldDb.filter(product => product.ID !== req.params.id )
+    let product = oldDb.find(product => product.ID === req.params.id )
+    let modifiedProduct = { ...product };
+   
+    modifiedProduct.image = `http://localhost:3001/img/${req.params.id + path.extname(req.file.originalname)}`;
+    newDb.push(modifiedProduct);
+    await writeDB(ProductFilePath, newDb);
+    res.send({ id: req.params.id });
     res.send("ok");
     //   }
 
